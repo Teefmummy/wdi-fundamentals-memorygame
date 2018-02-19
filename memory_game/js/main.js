4eda0c2d835b642b6ea97b5e58ae5003bcfe7c8d
Andrew Bryner <bryner.and@gmail.com>
2018-02-19 16:04:37 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,21 @@
 console.log('up and running');
 
-var cardOne = 'queen';
-var cardTwo = 'queen';
-var cardThree = 'king';
-var cardFour = 'king';
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
 
-console.log ('User flipped ' + cardOne);
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log('User flipped ' + cardOne);
+console.log('User flipped ' + cardTwo);
+
+if (cardsInPlay.length === 2){
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		alert('You Found a Match!');
+	}
+	else {
+		alert('Sorry, try again');
+	}
+}
