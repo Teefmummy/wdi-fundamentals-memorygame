899fc7217cb0ca5ab2ff1d9341b21fd17b0b033f
Andrew Bryner <bryner.and@gmail.com>
2018-02-25 15:55:46 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,29 +1,45 @@
 console.log('up and running');
 
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = [
+	{ 
+	rank: 'queen',
+	suit: 'hearts',
+	cardImage: 'images/queen-of-hearts.png'
+	},
+	{ 
+	rank: 'queen',
+	suit: 'diamonds',
+	cardImage: 'images/queen-of-diamonds.png'
+	},
+	{ 
+	rank: 'king',
+	suit: 'hearts',
+	cardImage: 'images/king-of-hearts.png'
+	},
+	{ 
+	rank: 'king',
+	suit: 'diamonds',
+	cardImage: 'images/king-of-diamonds.png'
+	}
+];
+
+
 var cardsInPlay = [];
-// var cardOne = cards[0];
-// var cardTwo = cards[2];
+
 var checkForMatch = function(){
 	if (cardsInPlay[0] === cardsInPlay[1]){
-		console.log("You found a Match!");
+		alert("You found a Match!");
 	} else {
-		console.log("Sorry, Try Again");
+		alert("Sorry, Try Again");
 	}
 }
 
 var flipCard = function(cardId){
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 	checkForMatch();
-
-	// if (cardsInPlay.length === 2){
-	// 	if (cardsInPlay[0] === cardsInPlay[1]){
-	// 		alert('You Found a Match!');
-	// }
-	// else {
-	// 	alert('Sorry, try again');
-	// }
 }
 
 flipCard(0);
